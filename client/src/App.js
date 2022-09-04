7aa8b075ba96f54c38b54c029bbf6325c4eaa341
hisham <hishammpsn@gmail.com>
2022-09-04 14:58:07 +0530

    added header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import styles from "./styles";
 
 function App() {
   return (
-    <div className="bg-black" >
-      Hello world
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.flexCenter} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+        <Header />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Librery from "../Assets/library-svgrepo-com (1).svg";
+import styles from "../styles";
+function Header() {
+  return (
+    <div className="text-white font-poppins flex p-4 items-center justify-between ">
+      <div className="w-full flex items-center ">
+        <div className="text-white ss:text-[40px]  text-[20px] mr-1  ss:mr-4 ">
+          <span className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-pink-600  to-blue-400" >Library</span>{" "}
+        </div>
+        <img src={Librery} alt="library" className=" mt-2 w-[20px] ss:w-[40px] h-[45px]" />
+      </div>
+      <div
+        className={`${styles.flexCenter} w-full h-[35px] p-1 justify-evenly  flex bg-primary border  border-blue-300 rounded-lg`}
+      >
+        <i  class="fa-solid fa-magnifying-glass"></i>
+        <input
+          type="text"
+          className="bg-primary border-gray-100 w-full outline-none text-[15px] "
+          placeholder=" search here..."
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
